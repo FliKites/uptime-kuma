2c04cf52969c99205179a3a7293a51c898148b6b
FliKites <72367027+FliKites@users.noreply.github.com>
2023-05-21 22:46:15 -0600

    Update database.js
    Add port env varaible

--- server/database.js
+++ server/database.js
@@ -130,6 +130,7 @@ class Database {
         R.useBetterSQLite3 = false;
         const con = {
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
